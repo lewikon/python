4863a771f6ac31e1eb7d136c6160bb1a1d2bdf6c
Alina <lewibein@icloud.com>
2022-09-05 19:01:28 +0300

    simple decision

--- task_3.py
+++ task_3.py
@@ -1,14 +1,3 @@
 n = int(input('number: '))
-m = n * -1
-if (n < 0):
-    print (n)
-    while(n < m):
-        n = n+1
-        print(n)
-elif (n > 0):
-    print(n)
-    while(n > m):
-        n = n-1
-        print(n)
-else:
-    print(n)
+for i in range(-n, n+1):
+    print(i)
---
+++ task_4.py
@@ -0,0 +1,6 @@
+a = float(input('number: '))
+print(a)
+while(a > 0):
+    b = a % 10
+    a = a/10
+print(b)
