7bd768488c1d1a3ed84549c308e43ae2376d515d
Alina <lewibein@icloud.com>
2022-09-05 19:48:44 +0300

    aaaaa

--- task_4.py
+++ task_4.py
@@ -2,3 +2,6 @@ a = float(input('number: '))
 result = int(a*10%10)
 print(result)
 
+number = input()
+if '.' in number:
+    print(number[number.index('.')+1])
---
+++ task_5.py
@@ -0,0 +1,5 @@
+n = int(input('number: '))
+if(n%5 == 0 or n%10 == 0 or n%15 == 0 and n%30 != 0):
+    print('yes')
+else:
+    print('no')
