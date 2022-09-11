36427717fcdd3cdaf894720d0dba1e1512d13b03
Alina <lewibein@icloud.com>
2022-09-11 21:43:19 +0300

    task_1

---
+++ seminar_3/homework_3/task_1.py
@@ -0,0 +1,9 @@
+from operator import index
+
+
+some_list = [input() for _ in range(int(input('Введите кол-во элементов: ')))]
+print(some_list)
+summa = 0
+for i in range(0,len(some_list),2):
+    summa += int(some_list[i])
+print(summa)
