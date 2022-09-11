03ab4d47b887761a46eba19a7d82b3f324968897
Alina <lewibein@icloud.com>
2022-09-11 14:43:45 +0300

    input in 1string

--- seminar_3/task_2.py
+++ seminar_3/task_2.py
@@ -1,6 +1,5 @@
 from curses.ascii import isdigit
 
-
 some_list = input('введите строки через запятую: ').split(',')
 print(some_list)
 n = input('введите искомое число: ')
@@ -9,3 +8,10 @@ for i in some_list:
         print('yes')
     else:
         print('no')
+
+# some_list = [input() for _ in range(int(input('Введите кол-во элементов: ')))]
+# n = int(input('Введите число: '))
+# if str(n) in some_list:
+#     print('Да')
+# else:
+#     print('Нет')
