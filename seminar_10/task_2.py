415adccdd524b4a8d5b1e8c3ea7d48fa5db8ffd6
Alina <lewibein@icloud.com>
2022-10-09 10:40:30 +0300

    bigbell

--- seminar_10/task_2.py
+++ seminar_10/task_2.py
@@ -1,2 +1,16 @@
 class BigBell:
+    def __init__(self):
+        self.count = 0
     
+    def sound(self):
+        if self.count % 2 == 0:
+            print('ding')
+        else:
+            print('dong')
+        self.count +=1
+
+bell = BigBell()
+bell.sound()
+bell.sound()
+bell.sound()
+            
