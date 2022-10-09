0c1d28f674865e8508f16530cddf5eba58823287
Alina <lewibein@icloud.com>
2022-10-09 16:16:49 +0300

    traffL

--- seminar_10/homework/task_1.py
+++ seminar_10/homework/task_1.py
@@ -2,17 +2,16 @@ from time import sleep
 
 class TrafficLight:
     def __init__(self):
-        self.__color = 'red'
+        self.__color__ = 'red'
     
     def running(self):
-        print("\033[31m {}" .format(self.__color))
+        print("\033[31m {}" .format(self.__color__))
         sleep(7)
-        self.__color = 'yellow'
+        self.__color__ = 'yellow'
         sleep(3)
-        print("\033[33m {}" .format(self.__color))
-        self.__color = 'green'
+        print("\033[33m {}" .format(self.__color__))
+        self.__color__ = 'green'
         sleep(7)
-        print("\033[32m {}" .format(self.__color))
-
+        print("\033[32m {}" .format(self.__color__))
 light =TrafficLight()
 light.running()
